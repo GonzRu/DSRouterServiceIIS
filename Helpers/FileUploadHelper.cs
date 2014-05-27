d17bbdf27eeb242c24d6b1333804e406d3beb5ff
Smirnov Denis <gonzru@gmail.com>
2014-05-27 16:39:00 +0400

    add FileUploadHelper class

---
+++ Helpers/FileUploadHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DSFakeService.DataSources;
+
+namespace DSRouterService.Helpers
+{
+    public class FileUploadHelper
+    {
+        #region Constructor
+
+        public FileUploadHelper(DataServersCollector dataSource)
+        {
+            
+        }
+
+        #endregion
+    }
+}
