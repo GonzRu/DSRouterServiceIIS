3a47a6dfed5cb776c90e2721cd412015010d8a90
Smirnov Denis <gonzru@gmail.com>
2014-09-22 16:06:37 +0400

    add DataSetModel for reports

---
+++ Reports/ReportsDataSource.cs
@@ -0,0 +1,13 @@
+﻿namespace Reports {
+    
+    
+    public partial class ReportsDataSource {
+        partial class TagsDescriptionDataTable
+        {
+        }
+    
+        partial class FixTrendsValuesDataTable
+        {
+        }
+    }
+}
